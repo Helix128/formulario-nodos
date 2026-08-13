cba479a3ce93f27d78966bc658b73f91c84f1c11
Helix128 <diegoatari8@gmail.com>
2026-08-12 20:40:01 -0400

    fix puerto

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "formulario-nodos",
       script: "./.venv/bin/gunicorn",
-      args: "app.main:app -k uvicorn.workers.UvicornWorker --workers 2 --bind 127.0.0.1:8000",
+      args: "app.main:app -k uvicorn.workers.UvicornWorker --workers 2 --bind 127.0.0.1:8104",
       cwd: __dirname,
       interpreter: "none",
       env: { PYTHONUNBUFFERED: "1" },
